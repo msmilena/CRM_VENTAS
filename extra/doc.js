f2b84766ee9f7f2b60a52c4ac06f84da509b51d4
Milena Mitac <milena.mitac@unmsm.edu.pe>
2023-03-04 21:21:17 -0500

    Avance vistas

---
+++ extra/doc.js
@@ -0,0 +1,22 @@
+/**Ventana ver producto */
+function abrir() {
+  document.getElementById("vent").style.display = "flex";
+}
+function cerrar() {
+  document.getElementById("vent").style.display = "none";
+}
+
+/*carrito*/
+function abrircarrito() {
+  document.getElementById("carrito").style.display = "flex";
+}
+function cerrarcarrito() {
+  document.getElementById("carrito").style.display = "none";
+}
+
+/* Búsqueda */
+var btn_expandir = document.querySelector(".texto_busqueda_avanzada");
+var busqueda_avanzada = document.querySelector(".busqueda_avanzada");
+btn_expandir.addEventListener("mousedown", function () {
+  busqueda_avanzada.classList.toggle("hidden");
+});
