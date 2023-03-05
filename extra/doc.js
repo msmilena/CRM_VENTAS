1cbff57d5c3f5396b24a76b85fcc36ba8c8345d1
Milena Mitac <milena.mitac@unmsm.edu.pe>
2023-03-05 01:46:04 -0500

    Cambios vistas clientes vendedor

--- extra/doc.js
+++ extra/doc.js
@@ -13,12 +13,27 @@ function abrircarrito() {
 function cerrarcarrito() {
   document.getElementById("carrito").style.display = "none";
 }
+/*Boleta*/
+function abrircomprobante() {
+  document.getElementById("boleta-comprobante").style.display = "flex";
+}
+function cerrarcomprobante() {
+  document.getElementById("boleta-comprobante").style.display = "none";
+}
+
+/*Cotización*/
+function abrircotizacion() {
+  document.getElementById("boleta-cotizacion").style.display = "flex";
+}
+function cerrarcotizacion() {
+  document.getElementById("boleta-cotizacion").style.display = "none";
+}
 
 /* Búsqueda */
-var btn_expandir = document.querySelector('.texto_busqueda_avanzada');
-var busqueda_avanzada = document.querySelector('.busqueda_avanzada');
+var btn_expandir = document.querySelector(".texto_busqueda_avanzada");
+var busqueda_avanzada = document.querySelector(".busqueda_avanzada");
 if (btn_expandir && busqueda_avanzada) {
-  btn_expandir.addEventListener('mousedown', function () {
-    busqueda_avanzada.classList.toggle('hidden');
+  btn_expandir.addEventListener("mousedown", function () {
+    busqueda_avanzada.classList.toggle("hidden");
   });
 }
